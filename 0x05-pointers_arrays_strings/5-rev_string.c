d02adeab3976b4b429090609b10aae12b3dafa31
Mawuli Agropah <wizzyagrops2016@gmail.com>
2023-06-19 21:57:35 +0000

    done correction

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,23 +6,22 @@
  */
 void rev_string(char *s)
 {
-	char tmp;
-	int i, len, len1;
+	int i, j;
+	int count = 0;
+	int length;
+	int temp;
 
-	len = 0;
-	len1 = 0;
-
-	while (s[len] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		len++;
+		count++;
 	}
 
-	len1 = len - 1;
-
-	for (i = 0; i < len / 2; i++)
+	length = count;
+	
+	for (j = 0; j < length / 2; j++)
 	{
-		tmp = s[i];
-		s[i] = s[len1];
-		s[len1--] = tmp;
+		temp = s[j];
+		s[j] = s[length - 1 - j];
+		s[length - 1 - j] = s[j];
 	}
 }
